1516f81010e4cd647be25baaef8c20d08047e39e
Frontend JavaScript <frontendjavascript@iMAC-13.local>
2025-04-10 20:42:14 +0300

    curs4

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './assets/scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import './js/main.js';
-import './js/nav_sticky.js';
+import './js/nav_sticky.js'; 
